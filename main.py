e8d65c3b9d6522ef28480873b198194635477673
Kate Liang <kateliangg@gmail.com>
2024-04-20 17:02:09 -0400

    Fix prompt

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ llm = OpenAI(temperature=0, api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
 
 PROMPT = """ 
 Given an input question, first create a syntactically correct postgresql query to run,  
-then look at the results of the query and return the answer.  
+then look at the results of the query and return the answer. Remove the ``` in the response.  
 The question: {question}
 """
 
 db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True, top_k=3)
-question = "what is the average rent price in chicago in nov 2022 according to redfin?" 
+question = "what is the most common order that customers place?" 
 # use db_chain.run(question) instead if you don't have a prompt
 db_chain.run(PROMPT.format(question=question))
